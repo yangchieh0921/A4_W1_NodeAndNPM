2ce9a75a77a72024ffe46305be5cf0a66fa751b3
Chieh Yang <yang@Chieh.local>
2021-03-16 02:22:02 -0400

    change

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-const express = require('express'); // like a PHP require 
-const path = require('path');
-const server = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// set our views directory
-server.set("views", path.join(__dirname, 'views'));
-// set the static assets director so Express knows where to look
-// for css file, JS files, images et - anything static
-server.use(express.static(path(__dirname, "public")));
+var app = express();
 
-server.get("/", (req, res) => {
-    console.log('you have now hit the home route');
-    res.sendFile('views/index.html');
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-server.get("/contact", (req, res) => {
-    console.log('hit the contact route');
-    res.sendFile('views/contact.html');
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000; // localhost:3000
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-server.listen(port, () => {
-    console.log(`Server is now running at ${port}/`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

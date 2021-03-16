0b9d4e7c1275a2ed3a922ccaca4b7d675ed74dfe
Chieh Yang <yang@Chieh.local>
2021-03-15 23:12:28 -0400

    adding express

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-const http = require('http'); // like a PHP require 
+const express = require('express'); // like a PHP require 
 
-// require is more or less the same as a JS import
-//const hostname = '127.0.0.1'; // this is localhost
 const port = process.env.PORT || 3000; // localhost:3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server is now running at ${port}/`);
 });

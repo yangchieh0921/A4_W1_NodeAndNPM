423989760344cf0f27984364a824fd9c8a322ed0
Chieh Yang <yang@Chieh.local>
2021-03-16 02:36:01 -0400

    change

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# A4_W3_nodeAndNPM
+# A4_W1_NodeAndNPM
--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express'); //like a PHP require 
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const server = express();
 
-var app = express();
+// set our views directory
+server.set("views", path.join(__dirname, 'views'));
+// set the static assets director so Express knows where to look
+// for css files, JS files, images et - anything static
+server.use(express.static(path.join(__dirname, 'public')));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+server.get("/", (req, res) => {
+    console.log('you have now hit the home route');
+    res.sendFile(path.join(__dirname, 'views/index.html')); // localhost:3000/views/index.html
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+server.get("/contact", (req, res) => {
+    console.log('hit the contact route');
+    res.sendFile(path.join(__dirname, 'views/contact.html'));
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const port = process.env.PORT || 3000; // localhost:3000
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+server.listen(port, () => {
+  console.log(`Server is now running at ${port}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;

9311b78337182dee34cde283f52386b4fa4c9668
Chieh Yang <yang@Chieh.local>
2021-03-15 23:17:26 -0400

    fix error

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server is noew running at ${port}/`);
 });
